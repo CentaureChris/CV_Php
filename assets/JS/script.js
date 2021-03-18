35a71177f9fa3582dde2252f049a52458e6abf43
CentaureChris <c.centaure972@gmail.com>
2021-03-18 12:12:51 +0100

    add headerUser and contact

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,30 +1,32 @@
-$(document).ready(function(){
-    function animation(){
-        $('#look')
-            .css('position','relative')
-            .animate({
-                left: '30%',
-            },2000);
-        $('#look')
-            .delay(50)
-            .css('position','relative')
-            .animate({
-                    left:'0%',
-            },2000)
-    }
-    function animation2(){
-        $('#setAnim')
-            .css('position','relative')
-            .animate({
-                left: '330%',
-            },5000);
-        $('#setAnim')
-            .delay(110)
-            .css('position','relative')
-            .animate({
-                    left:'50%',
-            },2000)
-        }
-    setInterval(animation,1000);
-    setInterval(animation2,1000);
-    }); 
+$(document).ready(function () {
+  function animation() {
+    $("#look").css("position", "relative").animate(
+      {
+        left: "30%",
+      },
+      2000
+    );
+    $("#look").delay(50).css("position", "relative").animate(
+      {
+        left: "0%",
+      },
+      2000
+    );
+  }
+  function animation2() {
+    $("#setAnim").css("position", "relative").animate(
+      {
+        left: "200%",
+      },
+      5000
+    );
+    $("#setAnim").delay(110).css("position", "relative").animate(
+      {
+        left: "50%",
+      },
+      2000
+    );
+  };
+  setInterval(animation, 1000);
+  setInterval(animation2, 1000);
+});
